d7a00ccb307d36a220625e83f74d588338eca122
Peter Gerhard <peter.gerhard90@gmail.com>
2015-05-09 16:59:04 +0200

    implement getproducts action

--- app/controllers/ProductsController.scala
+++ app/controllers/ProductsController.scala
@@ -1,14 +1,18 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.ProductsService
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class ProductsController(service: ProductsService) extends Controller {
 
   def getProducts() = Action.async {
-    Future(Ok)
+    service.getProducts() map {
+      case products: List[Product] => Ok(Json.toJson(products))
+    } recover {
+      case e: Exception => InternalServerError(e.getMessage)
+    }
   }
 
 }
--- app/services/ProductsService.scala
+++ app/services/ProductsService.scala
@@ -2,13 +2,15 @@ package services
 
 import common.sphere.SphereClient
 import io.sphere.sdk.products.Product
+import io.sphere.sdk.products.queries.ProductQuery
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
 class ProductsService(sphereClient: SphereClient) {
 
-  def getProducts(): Future[Seq[Product]] = {
-    Future(Seq())
+  def getProducts(): Future[List[Product]] = {
+    sphereClient.execute(ProductQuery.of()) map (res => res.getResults.asScala.toList)
   }
 }
