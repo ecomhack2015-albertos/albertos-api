cadd3406cc914105c899669b1135a511b065680f
Peter Gerhard <peter.gerhard90@gmail.com>
2015-05-09 17:54:10 +0200

    added product to food conversion

--- app/common/models/Food.scala
+++ app/common/models/Food.scala
@@ -1,9 +1,23 @@
 package common.models
 
+import io.sphere.sdk.products.Product
 import play.api.libs.json.Json
 
-case class Food(name: String, price: Double)
+case class Food(id: String, Version: Long, name: String, price: Double)
 
-object Food {
+object Food extends ModelUtils {
   implicit val personFormat = Json.format[Food]
+
+  def fromProduct(product: Product): Option[Food] = {
+    val variant = product.getMasterData.getStaged.getMasterVariant
+
+    try {
+      Option(Food(
+        product.getId,
+        product.getVersion,
+        readStringAttribute(variant, "name"),
+        readDoubleAttribute(variant, "price")
+      ))
+    } catch { case e: Exception => None } // Todo Try[Food]
+  }
 }
---
+++ app/common/models/ModelUtils.scala
@@ -0,0 +1,20 @@
+package common.models
+
+import io.sphere.sdk.attributes.AttributeAccess
+import io.sphere.sdk.products.ProductVariant
+
+import scala.collection.JavaConverters._
+
+trait ModelUtils {
+  def getAttribute(variant: ProductVariant, name: String) =
+    variant.getAttribute(name).get()
+
+  def readStringAttribute(variant: ProductVariant, name: String) =
+    getAttribute(variant, name).getValue(AttributeAccess.ofString().attributeMapper())
+
+  def readDoubleAttribute(variant: ProductVariant, name: String) =
+    getAttribute(variant, name).getValue(AttributeAccess.ofDouble().attributeMapper())
+
+  def readMoneyAttribute(variant: ProductVariant, name: String) =
+    getAttribute(variant, name).getValue(AttributeAccess.ofMoney().attributeMapper())
+}
